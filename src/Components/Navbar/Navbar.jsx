4b9f8ff1f9ddc5cdaf3a36c377ec6d039a19426c
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-10-23 17:44:52 +0200

    created site

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+	return (
+		<div className="navbar">
+			<div className="nav-logo">
+				<img src={logo} alt="" />
+				<p>SHOPPER</p>
+			</div>
+			<ul className="nav-menu">
+				<li>Shop</li>
+				<li>Menu</li>
+				<li>Woman</li>
+				<li>Kids</li>
+			</ul>
+			<div className='nav-login-cart'>
+				<button>Login</button>
+				<img src={cart_icon}/>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
