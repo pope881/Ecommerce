e7b57e44d04ad3a1ae12e9456724d1a819505042
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-10-24 13:53:47 +0200

    navbar updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,14 +10,17 @@ const Navbar = () => {
 				<p>SHOPPER</p>
 			</div>
 			<ul className="nav-menu">
-				<li>Shop</li>
-				<li>Menu</li>
+				<li>
+					Shop <hr />
+				</li>
+				<li>Men</li>
 				<li>Woman</li>
 				<li>Kids</li>
 			</ul>
-			<div className='nav-login-cart'>
+			<div className="nav-login-cart">
 				<button>Login</button>
-				<img src={cart_icon}/>
+				<img src={cart_icon} />
+				<div className="nav-cart-count"></div>
 			</div>
 		</div>
 	)
