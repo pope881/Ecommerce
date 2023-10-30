4626ad3e9bd126c33975caee7ff67cf6919cb62c
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-10-30 17:39:46 +0100

    shop context added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
+import Newsletter from '../Components/Newsletter/Newsletter'
 
 const Shop = () => {
 	return (
@@ -11,6 +12,7 @@ const Shop = () => {
 			<Popular />
 			<Offers />
 			<NewCollections />
+			<Newsletter />
 		</div>
 	)
 }
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 
-const ShopCategory = params => {
-	return <div></div>
+const ShopCategory = props => {
+	const { all_product } = useContext(ShopContext)
+	return (
+		<div className="shop-category">
+			<img src={props.banner} alt="" />
+			<div className="shopcategoty-indexSort">
+				<p>
+					<span>Showiing 1-12</span> out of 36 products
+				</p>
+				<div className="shopcategory-sort">
+					Sort by <img src={dropdown_icon} alt="" />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ShopCategory
