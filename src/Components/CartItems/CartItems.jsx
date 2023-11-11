50adb9900e7d723182df12c11ab4be20564175f9
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-11-11 12:10:06 +0100

    cartitems updated

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -65,7 +65,7 @@ const CartItems = () => {
 					<button>PROCEED TO CHECKOUT</button>
 				</div>
 				<div className="cartitems-promocode">
-					<p>If you have a promo code, Enter it here</p>
+					<p>If you have a promo code, enter it here</p>
 					<div className="cartitems-promobox">
 						<input type="text" placeholder="promo code" />
 						<button>Submit</button>
