6e152a118cba4fe1e6a692553f39d87f70c8f5ac
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-11-20 12:42:46 +0100

    login updated

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { authActions } from '../Components/store/auth'
 
 import './CSS/LoginSignup.css'
-import { Link, redirect, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const LoginSignup = () => {
+	// const [enteredEmail, setEnteredEmail] = useState('')
+	// const [enteredPassword, setEnteredPassword] = useState('')
+	const [enteredValues, setEnteredValues] = useState({ email: '', password: '' })
+
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 
@@ -12,24 +16,65 @@ const LoginSignup = () => {
 		navigate('/')
 	}
 
-	const loginHandler = event => {
+	const handleSubmit = event => {
 		event.preventDefault()
 
 		dispatch(authActions.login())
+		// console.log(enteredEmail, enteredPassword)
+		console.log(enteredValues);
 		navigateHandler()
 	}
 
+	function handleInputChange(identifier, value) {
+		setEnteredValues(prevValues => ({ ...prevValues, [identifier]: value }))
+	}
+
+	// function handleEmailChange(event) {
+	// 	setEnteredEmail(event.target.value)
+	// }
+
+	// function handlePasswordChange(event) {
+	// 	setEnteredPassword(event.target.value)
+	// }
+
 	return (
-		<form onSubmit={loginHandler}>
-			<div>
-				<label htmlFor="email">Email</label>
-				<input type="email" id="email" />
+		<form onSubmit={handleSubmit}>
+			<h2>Login</h2>
+
+			<div className="control-row">
+				<div className="control no-margin">
+					<label htmlFor="email">Email</label>
+					<input
+						id="email"
+						type="email"
+						name="email"
+						// onChange={handleEmailChange}
+						onChange={event => handleInputChange('email', event.target.value)}
+						// value={enteredEmail}
+						value={enteredValues.email}
+					/>
+				</div>
+
+				<div className="control no-margin">
+					<label htmlFor="password">Password</label>
+					<input
+						id="password"
+						type="password"
+						name="password"
+						// onChange={handlePasswordChange}
+						onChange={event => handleInputChange('password', event.target.value)}
+						// value={enteredPassword}
+						value={enteredValues.password}
+					/>
+				</div>
 			</div>
-			<div>
-				<label htmlFor="password">Password</label>
-				<input type="password" id="password" />
-			</div>
-			<button>Login</button>
+
+			<p className="form-actions">
+				<button type="button" className="button button-flat">
+					Reset
+				</button>
+				<button className="button">Login</button>
+			</p>
 		</form>
 	)
 }
