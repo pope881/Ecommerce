fbe148dd1c5f9f113fb1bbaed0dd0aad7ab1ce0c
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-12-07 19:23:41 +0100

    ContactForm updated

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -5,11 +5,9 @@ const ContactForm = () => {
 		<section className={classes.contact}>
 			<div className={classes['contact-content']}>
 				<h2>Contact Us</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ratione eius blanditiis suscipit accusamus,
-					deleniti asperiores, obcaecati,
-				</p>
+				<p>We can't solve your problem if you don't tell us about it !</p>
 			</div>
+
 			<div className={classes['contact-container']}>
 				<div className={classes['contact-container-contactInfo']}>
 					<div className={classes['contact-container-contactInfo-box']}>
@@ -18,9 +16,9 @@ const ContactForm = () => {
 						</div>
 						<div className={classes['contact-container-contactInfo-box-text']}>
 							<h3>Address</h3>
-							<p>4671 Sugar Camp Road, </p>
-							<p> Owatonna,Minesota,</p>
-							<p>55060</p>
+							<p> Starołęcka Street 33 </p>
+							<p> Poznań, Greater Poland</p>
+							<p>Poland</p>
 						</div>
 					</div>
 					<div className={classes['contact-container-contactInfo-box']}>
@@ -38,10 +36,18 @@ const ContactForm = () => {
 						</div>
 						<div className={classes['contact-container-contactInfo-box-text']}>
 							<h3>Email</h3>
-							<p>ewk@wp.pl</p>
+							<a href="mailto:shopper@gmail.com">shopper@gmail.com</a>
 						</div>
 					</div>
 				</div>
+				<div className={classes['contact-container-map']}>
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.9738718838817!2d16.93186871252162!3d52.370893771905806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47045aee1edd4ceb%3A0xc03dcd73ba68a0d7!2zU3Rhcm_FgsSZY2thIDMzLCA2MS0zNjEgUG96bmHFhA!5e0!3m2!1spl!2spl!4v1701961446805!5m2!1spl!2spl"
+						allowfullscreen=""
+						loading="lazy"
+						referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+
 				<div className={classes['contact-container-contactForm']}>
 					<form action="">
 						<h2>Send Message</h2>
