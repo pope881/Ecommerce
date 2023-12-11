d53d2b54a00bdf3882b5b6afcfa78fa2afa2cc14
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-12-11 19:37:33 +0100

    aboutUs updated

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,13 +1,13 @@
 import about_img from '../Assets/about-6.png'
-import './AboutUs.css'
+import classes from './AboutUs.module.css'
 
 const AboutUs = () => {
 	return (
-		<section className="about-us">
-			<div className="main-img">
+		<section className={classes.aboutUs}>
+			<div className={classes['aboutUs-mainImg']}>
 				<img src={about_img} alt="" />
 			</div>
-			<div className="text">
+			<div className={classes['aboutUs-text']}>
 				<h4>About</h4>
 				<h1>
 					We Guarantee Your Business <br /> Success - Love to Help you
@@ -16,14 +16,17 @@ const AboutUs = () => {
 				<p>
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quis natus dolores tempore dolorem
 					reprehenderit quae laborum. Vero aliquid, aut ab est nostrum rerum fugit! Ex saepe provident illum nostrum.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat facilis, blanditiis voluptatibus laborum
+					optio saepe veniam, neque explicabo deserunt exercitationem quos similique. Aliquid ratione ab ducimus
+					deleniti cupiditate eligendi dolor.
 				</p>
 
-				<div className="last-text">
-					<div className="text-1">
+				<div className={classes['aboutUs-lastText']}>
+					<div className={classes['aboutUs-lastText-text']}>
 						<h3>500+</h3>
 						<h5>Projects Done</h5>
 					</div>
-					<div className="text-1">
+					<div className={classes['aboutUs-lastText-text']}>
 						<h3>20k</h3>
 						<h5>Happy Clients</h5>
 					</div>
