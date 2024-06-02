93de4bed9a600d4e4eb8eae93393a4dfbfd29386
Bartosz Papierz <papierz.bartosz@gmail.com>
2024-06-02 17:49:49 +0200

    Hero.test.tsx has been updated

--- src/Components/Hero/Hero.test.tsx
+++ src/Components/Hero/Hero.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Hero } from './Hero'
 
