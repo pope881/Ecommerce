323a79841ce93625961806ece31d0327b208c856
Bartosz Papierz <papierz.bartosz@gmail.com>
2024-06-04 10:40:14 +0200

    router updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { CartProvider } from './Context/CartProvider'
 import { store } from './Components/store'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './theme'
-import { router } from './router.tsx'
+import { router } from './router2.tsx'
 
 function App() {
 	return (
